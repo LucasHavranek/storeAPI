aa0414985353416df68f4c871e052fa54462baaf
LucasHavranek <81269366+LucasHavranek@users.noreply.github.com>
2022-03-30 16:40:54 -0300

    First version storeAPI

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 import express from 'express'
+import cors from 'cors'
+import winston from 'winston'
+import clientsRouter from './routes/clientRoute.js'
+import productsRouter from './routes/productRoute.js'
+import salesRouter from './routes/saleRoute.js'
+import suppliersRouter from './routes/supplierRoute.js'
+
+const { combine, timestamp, label, printf } = winston.format
+const myFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level} ${message}`
+})
+
+global.logger = winston.createLogger({
+    level: 'silly',
+    transports: [
+        new(winston.transports.Console)(),
+        new(winston.transports.File)({ filename: 'storeAPI.log' })
+    ],
+    format: combine(
+        label({ label: 'storeAPI' }),
+        timestamp(),
+        myFormat
+    )
+})
 
 const app = express()
+app.use(express.json())
+app.use(cors())
+app.use('/client', clientsRouter)
+app.use('/product', productsRouter)
+app.use('supplier', suppliersRouter)
+app.use('/sale', salesRouter)
 
 app.listen(3000, () => console.log('API started'))
